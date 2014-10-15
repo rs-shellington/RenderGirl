8bc7a9696bdfe1bc2a5afe0d4245e8cc8fe2f4dc
Scott Shellington <scott.shellington@gmail.com>
2014-10-15 17:36:52 -0400

    Add pseudo code commit for intersection code.

--- src/com/greyhoundsoftware/raytrace/Triangle.java
+++ src/com/greyhoundsoftware/raytrace/Triangle.java
@@ -15,6 +15,41 @@ public class Triangle extends Plane {
 		
 		if(super.hasIntersection(ray)) {
 			
+			//intersection point 
+			double t = ray.getTmax();
+			Vector3D P = ray.point.add(ray.direction.scalarMultiply(t));
+			
+			/*
+			//
+		    // Step 2: inside-outside test
+		    //
+		 
+		    vector C; // vector perpendicular to triangle's plane
+		 
+		    // edge 0
+		    vector edge0 = v1 - v0;
+		    vector VP0 = P - v0;
+		    C = cross(edge0, VP0);
+		    if (dot(N, C) < 0)
+		        return false; // P is on the right side
+		 
+		    // edge 1
+		    vector edge1 = v2 - v1;
+		    vector VP1 = P - v1;
+		    C = cross(edge0, VP1);
+		    if (dot(N, C) < 0)
+		        return false; // P is on the right side
+		 
+		    // edge 2
+		    vector edge2 = v0 - v2;
+		    vector VP2 = P - v2;
+		    C = cross(CA, CP);
+		    if (dot(N, C) < 0)
+		        return false; // P is on the right side;
+		 
+		    return true; // this ray hits the triangle
+			*/
+			
 			return true;
 			
 		}
