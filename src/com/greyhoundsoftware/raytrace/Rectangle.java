305a3235364a224084731ce0b19d4ab9dcee4e54
Scott Shellington <scott.shellington@gmail.com>
2014-10-16 16:56:05 -0400

    Implement Rectangle

---
+++ src/com/greyhoundsoftware/raytrace/Rectangle.java
@@ -0,0 +1,25 @@
+package com.greyhoundsoftware.raytrace;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+public class Rectangle extends SceneObject {
+	
+	Triangle t1, t2;
+	
+	
+	public Rectangle(Vector3D ul, Vector3D ur, 
+				     Vector3D ll, Vector3D lr) {
+		
+		t1 = new Triangle(ul, ll, ur);
+		t2 = new Triangle(ur, ll, lr);
+		
+	}
+	
+	@Override
+	public boolean hasIntersection(Ray ray) {
+		
+		return t1.hasIntersection(ray) || t2.hasIntersection(ray);
+		
+	}
+
+}
